3e90351e2147e4e5f0832c4915e5dd58cbc5093a
Vivek Kumar <vivekk182btechcse2025@kccitm.edu.in>
2022-11-17 12:11:52 +0530

    basic stack

---
+++ stack/basicStack.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Stack
+{
+    int A[1000];
+    int end;
+
+public:
+    Stack()
+    {
+        end = 0;
+    }
+
+    void push(int val)
+    {
+        A[end] = val;
+        end++;
+        cout << "Pushed " << val << " at " << end - 1 << endl;
+    }
+
+    void top()
+    {
+        cout << A[end - 1] << " at " << end - 1 << endl;
+    }
+
+    void pop()
+    {
+        cout << "Deleted " << A[end - 1] << endl;
+        end--;
+    }
+
+    void size()
+    {
+        cout << "Size is " << end << endl;
+    }
+
+    bool isEmpty()
+    {
+        return end;
+    }
+};
+
+int main()
+{
+    Stack s;
+
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.push(4);
+    s.push(5);
+
+    cout << endl;
+
+    s.isEmpty();
+
+    cout << endl;
+
+    s.size();
+
+    cout << endl;
+
+    s.top();
+
+    cout << endl;
+
+    s.pop();
+}
