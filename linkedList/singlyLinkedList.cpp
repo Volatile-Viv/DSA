41726ec5efa35fe63379d7dd204d8942a1bfdceb
Vivek Kumar <vivekk182btechcse2025@kccitm.edu.in>
2023-02-05 22:41:08 +0530

    linked list

---
+++ linkedList/singlyLinkedList.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+};
+
+Node *insertAtHead(Node *head, int val)
+{
+    Node *temp = new Node();
+    temp->data = val;
+    temp->next = head;
+    head = temp;
+
+    return head;
+}
+
+Node *insertAtTail(int val)
+{
+    Node *temp = new Node();
+    temp->data = val;
+
+    return temp;
+}
+
+void traverse(Node *head)
+{
+    cout << "HEAD -> ";
+    while (head != NULL)
+    {
+        cout << head->data << " -> ";
+        head = head->next;
+    }
+    cout << "NULL";
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    Node *head = new Node();
+    Node *tail = new Node();
+
+    head = NULL;
+    tail = NULL;
+
+    for (int i = 0; i < n; i++)
+    {
+        int val;
+        cin >> val;
+
+        if (head == NULL)
+        {
+            head = insertAtTail(val);
+            tail = head;
+        }
+
+        else
+        {
+            tail->next = insertAtTail(val);
+            tail = tail->next;
+        }
+    }
+
+    traverse(head);
+
+    cout << endl;
+
+    cout << "HEAD -> " << head->data << endl;
+    cout << "TAIL -> " << tail->data;
+}
