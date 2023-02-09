143b748da6d8a7baee04b5a43e9ad90759d99b2c
Vivek Kumar <vivekk182btechcse2025@kccitm.edu.in>
2023-02-09 15:54:19 +0530

    added using linked list

---
+++ stack/stack_using_linked_list.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+};
+
+Node *push(int val, Node *head)
+{
+    Node *temp = new Node();
+    temp->data = val;
+    temp->next = head;
+
+    return temp;
+}
+
+void traverse(Node *head)
+{
+    while (head != NULL)
+    {
+        cout << head->data << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+Node *pop(Node *head)
+{
+    head = head->next;
+    return head;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    Node *head = NULL;
+
+    for (int i = 0; i < n; i++)
+    {
+        int val;
+        cin >> val;
+
+        head = push(val, head);
+    }
+
+    traverse(head);
+
+    cout << "After popping - " << endl;
+    head = pop(head);
+    traverse(head);
+}
