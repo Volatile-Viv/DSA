40f9bd17303cc322208a8d5b4f436d02e4bbf2c3
Vivek Kumar <vivekk182btechcse2025@kccitm.edu.in>
2023-03-18 19:08:32 +0530

    bubble sort

--- sorting/bubbleSort.cpp
+++ sorting/bubbleSort.cpp
@@ -14,9 +14,20 @@ int main()
 
     // algo
 
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (A[j] > A[j + 1])
+            {
+                int temp = A[j];
+                A[j] = A[j + 1];
+                A[j + 1] = temp;
+            }
+        }
+    }
 
-
-    //printing
+    // printing
 
     for (int i = 0; i < n; i++)
     {
