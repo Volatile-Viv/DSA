ff2e651350fac40d91b787f4ac4f8a1f13dfa5c1
Vivek Kumar <vivekk182btechcse2025@kccitm.edu.in>
2023-03-18 19:11:29 +0530

    merge sort

---
+++ sorting/mergeSort.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<int> A(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> A[i];
+    }
+
+    // algo
+
+    // printing
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << A[i] << " ";
+    }
+}
