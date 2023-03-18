b947d7c9c0251750456de19e24c9e42455d009d3
Vivek Kumar <vivekk182btechcse2025@kccitm.edu.in>
2023-03-18 18:47:52 +0530

    insertion sort

--- sorting/insertionSort.cpp
+++ sorting/insertionSort.cpp
@@ -3,4 +3,38 @@ using namespace std;
 
 int main()
 {
+    int n;
+    cin >> n;
+
+    vector<int> A(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> A[i];
+    }
+
+    // algo
+
+    for (int i = 1; i < n; i++)
+    {
+        int temp = A[i];
+        int j;
+        for (j = i - 1; j >= 0; j--)
+        {
+            if (A[j] > temp)
+            {
+                A[j + 1] = A[j];
+            }
+
+            else
+            {
+                break;
+            }
+        }
+        A[j + 1] = temp;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << A[i] << " ";
+    }
 }
