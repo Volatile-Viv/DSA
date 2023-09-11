e01e5726000e5df85970850dac8952075d88c386
Vivek Kumar <vivekk182btechcse2025@kccitm.edu.in>
2023-09-11 15:16:33 +0530

    build Tree

--- binaryTree/binaryTree.cpp
+++ binaryTree/binaryTree.cpp
@@ -4,77 +4,43 @@ using namespace std;
 class Node
 {
 public:
-    int data;
+    int val;
     Node *left;
     Node *right;
 
-    Node(int d)
+    Node(int val)
     {
-        data = d;
-        left = NULL;
-        right = NULL;
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
     }
 };
 
-Node *create()
+Node *buildTree(Node *root)
 {
-    int d;
-    cout << "Enter the data: ";
-    cin >> d;
-    if (d == -1)
-    {
+    cout << "Enter the data --> ";
+    int val;
+    cin >> val;
+
+    root = new Node(val);
+
+    if (val == -1)
         return NULL;
-    }
-    Node *root = new Node(d);
 
-    cout << "Enter the data for left of " << d << ": " << endl;
-    root->left = create();
+    cout << "Enter the data for left Node - " << endl;
+    root->left = buildTree(root->left);
 
-    cout << "Enter the data for right of " << d << ": " << endl;
-    root->right = create();
+    cout << "Enter the data for right Node - " << endl;
+    root->right = buildTree(root->right);
 
     return root;
 }
 
-void levelOrderTraversal(Node *root)
-{
-    queue<Node *> q;
-    q.push(root);
-    q.push(NULL);
-
-    while (!q.empty())
-    {
-        Node *temp = q.front();
-        q.pop();
-
-        if (temp == NULL)
-        {
-            cout << endl;
-            if (!q.empty())
-            {
-                q.push(NULL);
-            }
-        }
-        else
-        {
-            cout << temp->data << " ";
-
-            if (temp->left)
-                q.push(temp->left);
-
-            if (temp->right)
-                q.push(temp->right);
-        }
-    }
-}
-
 int main()
 {
-    Node *root = create();
-    // root->left = new Node(2);
-    // root->right = new Node(3);
-    // root->left->left = new Node(4);
-    // root->left->right = new Node(5);
+    Node *root = NULL;
 
-    levelOrderTraversal(root);
+    root = buildTree(root);
+
+    cout << root->val;
 }
