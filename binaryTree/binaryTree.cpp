4acf9c58d513ee9b1ba3a3b0765f289a1ca6beb6
Vivek Kumar <vivekk182btechcse2025@kccitm.edu.in>
2023-09-12 12:20:46 +0530

    added Level Order Traversal

--- binaryTree/binaryTree.cpp
+++ binaryTree/binaryTree.cpp
@@ -36,11 +36,50 @@ Node *buildTree(Node *root)
     return root;
 }
 
+void levelOrderTraversal(Node *root)
+{
+    queue<Node *> q;
+    q.push(root);
+    q.push(NULL);
+
+    while (!q.empty())
+    {
+        Node *temp = q.front();
+        q.pop();
+
+        if (temp == NULL)
+        {
+            cout << endl;
+            if (!q.empty())
+            {
+                q.push(NULL);
+            }
+        }
+
+        else
+        {
+            cout << temp->val << " ";
+
+            if (temp->left)
+            {
+                q.push(temp->left);
+            }
+
+            if (temp->right)
+            {
+                q.push(temp->right);
+            }
+        }
+    }
+}
+
 int main()
 {
     Node *root = NULL;
 
     root = buildTree(root);
 
-    cout << root->val;
+    cout << "Level Order Traversal - " << endl;
+    // 1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1
+    levelOrderTraversal(root);
 }
